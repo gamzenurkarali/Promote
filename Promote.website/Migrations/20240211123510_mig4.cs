ad816cc95edb428775a13bc125db7385e658a98e
Hazar Kanat <hazarkanat@gmail.com>
2024-02-11 16:31:20 +0300

    İnformationForm

---
+++ Promote.website/Migrations/20240211123510_mig4.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Promote.website.Migrations
+{
+    public partial class mig4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "informationForms",
+                columns: table => new
+                {
+                    InformationFormId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmailAdress = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_informationForms", x => x.InformationFormId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "informationForms");
+        }
+    }
+}
